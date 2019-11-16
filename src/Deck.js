3616940502cb8d569c2be0a437a697e37f65c41b
2imad <imadyoussoufi@gmail.com>
2019-11-16 11:25:44 +0100

    get rotation with interpolation

--- src/Deck.js
+++ src/Deck.js
@@ -1,18 +1,53 @@
-import React, { Component } from 'react';
-import { View, Animated } from 'react-native';
+import React, { Component } from "react";
+import { View, Animated, PanResponder } from "react-native";
 
 class Deck extends Component {
+  constructor(props) {
+    super(props);
 
-   renderCards() {
-      return this.props.data.map(item => {
-         return this.props.renderCard(item)
-      })
-   }
-   render() {
-      return <View>
-         {this.renderCards()}
-      </View>
-   }
+    const position = new Animated.ValueXY();
+    const panResponder = PanResponder.create({
+      onMoveShouldSetPanResponder: () => true,
+      onPanResponderMove: (event, gesture) => {
+        const { dx, dy } = gesture;
+        position.setValue({ x: dx, y: dy });
+      },
+      onPanResponderRelease: () => {}
+    });
+    this.state = { panResponder, position };
+  }
+
+  getCardStyle = () => {
+    const { position } = this.state;
+    const rotate = position.x.interpolate({
+      inputRange: [-500, 0, 500],
+      outputRange: ["-120deg", "0deg", "120deg"]
+    });
+    return {
+      ...position.getLayout(),
+      transform: [{ rotate }]
+    };
+  };
+
+  renderCards() {
+    return this.props.data.map((item, index) => {
+      if (index === 0) {
+        return (
+          <Animated.View
+            key={item.id}
+            style={this.getCardStyle()}
+            {...this.state.panResponder.panHandlers}
+          >
+            {this.props.renderCard(item)}
+          </Animated.View>
+        );
+      }
+      return this.props.renderCard(item);
+    });
+  }
+  render() {
+    return <View>{this.renderCards()}</View>;
+  }
 }
 
 export default Deck;
